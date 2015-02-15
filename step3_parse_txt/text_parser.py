88e552ba5a74a987289742d61487715cc226c74c
John <*>
2015-02-15 03:43:40 -0500

    simpler folder name & other changes

--- step3_parse_txt/text_parser.py
+++ step3_parse_txt/text_parser.py
@@ -1,9 +1,12 @@
 from __future__ import division
 
+import json
 import os
 from os import listdir
 from os.path import basename, join, splitext
+import re
 
+import numpy as np
 import requests
 
 base_url = 'http://localhost:9000/api/parse/'
@@ -23,13 +26,21 @@ def parse_to_file(src_abs_filename, dest_abs_filename):
 	parsed = parse(content)
 
 	with open(dest_abs_filename, 'w') as dest:
-		dest.write(str(parsed))
-		
-def parse_to_dir(src_dir, dest_dir, src_filter = lambda f: f.endswith('.txt')):
+		dest.write(json.dumps(parsed))
+
+num_re = re.compile('lime_([0-9]+)_')
+def _lime_num(filename):
+	return int(num_re.findall(filename)[0])
+
+def parse_to_dir(src_dir, dest_dir, src_filter = lambda f: f.endswith('.txt'), bounds = [-np.inf, np.inf]):
 	if not os.path.exists(dest_dir):
 		os.makedirs(dest_dir)
 
-	src_files = [join(src_dir, f) for f in listdir(src_dir) if src_filter(f)]
+	def within_bounds(f):
+		f_num = _lime_num(f)
+		return f_num > bounds[0] and f_num < bounds[1]
+
+	src_files = [join(src_dir, f) for f in listdir(src_dir) if src_filter(f) and within_bounds(f)]
 	num_files = len(src_files)
 	for (i, s) in enumerate(src_files):
 		print('file: {}, percent_done: {}'.format(basename(s), i/num_files * 100))
