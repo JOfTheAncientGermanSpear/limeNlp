a93d8391fd6e61061777578eccd93e21f91cc3d0
LB User <joftheancientgermanspear@users.noreply.github.com>
2015-03-09 17:43:09 -0400

    sort step 4 by file name to facilate starting form particular file on crash

--- step4_make_features/tree_generator.py
+++ step4_make_features/tree_generator.py
@@ -133,6 +133,7 @@ def dir_to_trees(src_dir, dest_dir, src_filter = lambda f: f.endswith('.json')):
 		os.makedirs(dest_dir)
 
 	src_files = [join(src_dir, f) for f in listdir(src_dir) if src_filter(f)]
+        src_files = sorted(src_files)
 	num_files = len(src_files)
 	for (i, s) in enumerate(src_files):
 		print('file: {}, percent_done: {}'.format(basename(s), i/num_files * 100))
