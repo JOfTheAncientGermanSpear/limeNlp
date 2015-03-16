fa03cdcd55810a745a0f851d5cebb6f51bbd146a
John <*>
2015-03-16 13:05:38 -0400

    sanitize input text to be parsed

--- step3_parse_txt/text_parser.py
+++ step3_parse_txt/text_parser.py
@@ -15,8 +15,11 @@ import utils
 
 base_url = 'http://localhost:9000/api/parse/'
 
+def sanitize(text):
+	return text.replace('/', '%2f')
+
 def parse(text):
-	url = base_url + text.strip()
+	url = base_url + sanitize(text.strip())
 	response = requests.get(url)
 	return response.json()
 
