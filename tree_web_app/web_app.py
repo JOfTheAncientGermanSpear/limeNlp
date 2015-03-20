13b45221529c9d1009b875bfcb02bc81cb634f3f
LB User <joftheancientgermanspear@users.noreply.github.com>
2015-03-20 17:48:55 -0400

    attempting to draw trees

--- tree_web_app/web_app.py
+++ tree_web_app/web_app.py
@@ -1,15 +1,29 @@
+import json
+import sys
+
+import matplotlib
 import web
         
+sys.path.insert(0, '../step3_parse_txt/')
+sys.path.insert(0, '../step4_make_features/')
+import text_parser
+import tree_generator
+
+matplotlib.use('Agg')
+
 urls = (
-    '/(.*)', 'hello'
+    '/(.*)', 'file_maker'
 )
 app = web.application(urls, globals())
 
-class hello:        
-    def GET(self, name):
-        if not name: 
-            name = 'World'
-        return 'Hello1, ' + name + '!'
+class file_maker:        
+    def GET(self, text):
+        parse_res = text_parser.parse(text)
+        cpd = tree_generator.gen_from_json(parse_res)
+        files = ['corefs', 'phrase', 'deps']
+        for i in range(2):
+            tree_generator.print_tree(cpd[i], 'static/{}.ps'.format(files[i]))
+        return "<a href='static/corefs.ps'></a>"
 
 if __name__ == "__main__":
     app.run()
