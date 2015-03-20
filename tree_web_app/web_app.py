62db923c6a9221f0bea7fa72a064647f5212d4c2
John <*>
2015-03-20 18:07:27 -0400

    create link for each file

--- tree_web_app/web_app.py
+++ tree_web_app/web_app.py
@@ -1,15 +1,12 @@
 import json
 import sys
 
-import matplotlib
 import web
         
 sys.path.insert(0, '../step3_parse_txt/')
 sys.path.insert(0, '../step4_make_features/')
 import text_parser
 import tree_generator
-
-matplotlib.use('Agg')
 
 urls = (
     '/(.*)', 'file_maker'
@@ -21,9 +18,13 @@ class file_maker:
         parse_res = text_parser.parse(text)
         cpd = tree_generator.gen_from_json(parse_res)
         files = ['corefs', 'phrase', 'deps']
-        for i in range(2):
-            tree_generator.print_tree(cpd[i], 'static/{}.ps'.format(files[i]))
-        return "<a href='static/corefs.ps'></a>"
+        links = []
+        for i in range(3):
+            f = 'static/{}.ps'.format(files[i])
+            tree_generator.print_tree(cpd[i], f)
+            l = "<a href='" + f + "'>" + files[i] + "</a>"
+            links.append(l)
+        return "<br/>".join(links)
 
 if __name__ == "__main__":
     app.run()
