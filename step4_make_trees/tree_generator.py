90436da31559c2306ca77db5b01ca5908980b4d7
John <*>
2015-04-09 17:25:49 -0400

    indentation correction

--- step4_make_trees/tree_generator.py
+++ step4_make_trees/tree_generator.py
@@ -141,7 +141,7 @@ def dir_to_trees(src_dir, dest_dir, src_filter = lambda f: f.endswith('.json'), 
 		os.makedirs(dest_dir)
 
 	src_files = [join(src_dir, f) for f in listdir(src_dir) if src_filter(f) and utils.within_bounds(f, bounds)]
-        src_files = sorted(src_files)
+	src_files = sorted(src_files)
 	num_files = len(src_files)
 	for (i, s) in enumerate(src_files):
 		print('file: {}, percent_done: {}'.format(basename(s), i/num_files * 100))
