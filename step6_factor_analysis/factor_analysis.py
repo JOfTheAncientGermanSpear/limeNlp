d0a007789d371777138f915a621f91cc0bb83e81
john <joftheancientgermanspear@users.noreply.github.com>
2016-03-03 05:33:19 -0500

    lu features already saved in lexical/syntax files

--- step6_factor_analysis/factor_analysis.py
+++ step6_factor_analysis/factor_analysis.py
@@ -1,8 +1,5 @@
+import numpy as np
 import pandas as pd
-
-import sys
-sys.path.insert(0, '../utils/')
-import utils
 
 
 def _data_path(f):
@@ -22,9 +19,9 @@ def _remove_aphasia_dupe_cols(df):
             del df[c]
 
 
-def _load(dep_path=_data_path('dependencies'),
-          lex_path=_data_path('lexical'),
-          syn_path=_data_path('syntax')):
+def load(dep_path=_data_path('dependencies'),
+         lex_path=_data_path('lexical'),
+         syn_path=_data_path('syntax')):
 
     def rd(f):
         ret = pd.read_csv(f, index_col=0)
@@ -37,60 +34,6 @@ def _load(dep_path=_data_path('dependencies'),
         return ret
 
     return rd(dep_path), rd(lex_path), rd(syn_path)
-
-
-def _create_index_from_id(df):
-    """
-    :param df:
-    >>> import pandas as pd
-    >>> df = pd.DataFrame(dict(a = [1, 2, 3], id = ['lm201', 'lm1234', 'lm205']))
-    >>> _create_index_from_id(df)
-    >>> expected = {201, 1234, 205}
-    >>> actual = set(df.index)
-    >>> assert actual == expected
-    >>> assert len(df.index) == 3
-    >>> assert 'id' not in df
-    """
-    df.index = [utils.lime_num(i + '.txt') for i in df['id']]
-    del df['id']
-
-
-def _load_lu(lex_path=_data_path('lu_lexical'),
-             syn_path=_data_path('lu_syntax')):
-
-    def rd(f):
-        ret = pd.read_csv(f)
-        _create_index_from_id(ret)
-        return ret
-
-    return rd(lex_path), rd(syn_path)
-
-
-def load(deps_path=_data_path('dependencies'),
-         lex_path=_data_path('lexical'),
-         syn_path=_data_path('syntax'),
-         lu_lex_path=_data_path('lu_lexical'),
-         lu_syn_path=_data_path('lu_syntax')):
-
-    deps, lex, syn = _load(deps_path, lex_path, syn_path)
-    lu_lex, lu_syn = _load_lu(lu_lex_path, lu_syn_path)
-
-    def concat(left, right):
-        num_left_rows, num_left_cols = left.shape
-        num_right_rows, num_right_cols = right.shape
-
-        combined = pd.merge(left, right, left_index=True, right_index=True)
-        import pdb
-        pdb.set_trace()
-        _remove_aphasia_dupe_cols(combined)
-
-        return combined
-
-    lex_all = concat(lex, lu_lex)
-    syn_all = concat(syn, lu_syn)
-
-    return deps, lex_all, syn_all
-
 
 
 if __name__ == "__main__":
