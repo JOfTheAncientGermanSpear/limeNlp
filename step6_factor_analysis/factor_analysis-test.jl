f45481b32f6db658cf9b2f2399dea0b81b8869ed
john <joftheancientgermanspear@users.noreply.github.com>
2016-03-09 22:14:03 -0500

    code to plot PCA in results in julia

---
+++ step6_factor_analysis/factor_analysis-test.jl
@@ -0,0 +1,35 @@
+using Base.Test
+using DataFrames
+include("factor_analysis.jl")
+
+df = DataFrame(has_aphasia=[1, 2], has_aphasia_1=[1, 2])
+_remove_aphasia_dupe_cols(df)
+@test names(df) == [:has_aphasia]
+@test size(df) == (2, 1)
+df_diff = DataFrame(has_aphasia=[1, 2], has_aphasia_1=[4, 3])
+@test_throws AssertionError _remove_aphasia_dupe_cols(df_diff)
+
+
+df = DataFrame(one_four=@data([1, 2, 3, NA]), pure=@data([1, 2, 3, 4]))
+function in_thresh(col::Symbol, thresh::Float64)
+  in(col, names(_remove_na_cols(df, thresh)))
+end
+function in_thresh(col::Symbol)
+  in(col, names(_remove_na_cols(df)))
+end
+
+@test !in_thresh(:one_four, .76)
+@test in_thresh(:pure, .76)
+@test in_thresh(:one_four)
+@test !in_thresh(:pure, 1.0)
+
+
+df = DataFrame(a=@data([1, 2, 3, NA]), b=@data([1, 2, 3, 4]), c=@data([2, 4, NA, NA]))
+@test size(_remove_na_rows(df)) == (2, 3)
+@test size(_remove_na_rows(df, .6)) == (3, 3)
+
+
+df = DataFrame(a=@data([1, NA, 3]), b=@data([10, NA, 30]))
+df = _fillna(df)
+@test df[:a] == [1, 2, 3]
+@test df[:b] == [10.0, 20.0, 30.0]
---
+++ step6_factor_analysis/factor_analysis.jl
@@ -0,0 +1,137 @@
+using DataFrames
+using Gadfly
+using MultivariateStats
+
+
+function _data_path(f)
+  "../data/step5/$f.csv"
+end
+
+
+function _remove_aphasia_dupe_cols(df::DataFrame)
+  for c in names(df)
+    c_str = string(c)
+    if(startswith(c_str, "has_aphasia") & (c != :has_aphasia))
+      @assert df[:has_aphasia] == df[c]
+      delete!(df, c)
+    end
+  end
+end
+
+
+function _remove_na_cols(df::DataFrame, thresh::Float64=.7)
+  num_rows = size(df, 1)
+  max_nas = (1 - thresh) * num_rows
+
+  function within_limit(col::Symbol)
+    num_nas = sum(isna(df[col]))
+    ret::Bool = num_nas < max_nas
+    ret
+  end
+
+  cols_to_keep = filter(within_limit, names(df))
+  df[cols_to_keep]
+end
+
+
+function _remove_na_rows(df::DataFrame, thresh::Float64=.7)
+  num_cols = size(df, 2)
+  max_nas = (1 - thresh) * num_cols
+
+  function nas_cnt(row::DataFrameRow)
+    sum(isna([val for (sym, val) in row]))
+  end
+
+  rows_to_keep::Vector{Bool} = [nas_cnt(r) < max_nas for r in eachrow(df)]
+  df[rows_to_keep, :]
+end
+
+
+function _fillna(df::DataFrame, val_fn::Function=mean)
+  df = copy(df)
+  for c in names(df)
+    na_rows = isna(df[c])
+    val = val_fn(df[!na_rows, c])
+    df[na_rows, c] = val
+  end
+  df
+end
+
+
+function _is_aphasia_type_gen(aphasia_type::AbstractString)
+  pat_class = readtable("../data/patient_classifications.csv")
+  type_rows = pat_class[:aphasia_type] .== aphasia_type
+  pat_ids = Set(pat_class[type_rows, :id])
+
+  function select_ids(df::DataFrame)
+    Bool[in(id, pat_ids) for id in df[:id]]
+  end
+end
+
+
+function load_continuous(id_filter=df -> df[:id] .> 1000,
+                         dep_path=_data_path("dependencies"),
+                         lex_path=_data_path("lexical"),
+                         syn_path=_data_path("syntax"),
+                         col_thresh=Dict(:dep=>.7, :lex=>.7, :syn=>.7),
+                         row_thresh=Dict(:dep=>.7, :lex=>.7, :syn=>.7),
+                         fill_na_fn=mean)
+
+  function rd(f, cr_threshes)
+    ret = readtable(f)
+    rename!(ret, :x, :id)
+
+    ret = ret[id_filter(ret), :]
+
+    valid_rows = !(isna(ret[:has_aphasia]))
+    ret = ret[valid_rows, :]
+
+    _remove_aphasia_dupe_cols(ret)
+
+    c_thresh, r_thresh = cr_threshes
+
+    ret = _remove_na_cols(ret, c_thresh)
+    ret = _remove_na_rows(ret, r_thresh)
+    ret = _fillna(ret, fill_na_fn)
+
+    ret
+  end
+
+  function threshes(key)
+    (col_thresh[key], row_thresh[key])
+  end
+
+  Dict(:dep => rd(dep_path, threshes(:dep)),
+       :lex => rd(lex_path, threshes(:lex)),
+       :syn => rd(syn_path, threshes(:syn)))
+end
+
+
+function pca(dfs::Dict{Symbol, DataFrame},
+             lname::Symbol, rname::Symbol)
+  ldf_pca, rdf_pca = map([lname, rname]) do sym
+    df = dfs[sym]
+    mat::Matrix{Float64} = Matrix(df[:, 2:end])'
+    pca = fit(PCA, mat; maxoutdim=1)
+    recon_data::Vector{Float64} = transform(pca, mat)[:]
+    DataFrame(recon=recon_data, id=df[:id])
+  end
+  rename!(ldf_pca, :recon, lname)
+  rename!(rdf_pca, :recon, rname)
+
+  join(ldf_pca, rdf_pca, kind=:inner, on=:id)
+end
+
+
+function add_aphasia_classifications(df::DataFrame)
+  pat_class = readtable("../data/patient_classifications.csv")
+  join(df, pat_class, on=:id, kind=:inner)
+end
+
+
+function plot_(df::DataFrame, x_col::Symbol, y_col::Symbol)
+
+  df_plot = add_aphasia_classifications(df)
+  plot(df_plot, x=x_col, y=y_col, color=:aphasia_type,
+       Guide.Title("$y_col vs $x_col"))
+end
